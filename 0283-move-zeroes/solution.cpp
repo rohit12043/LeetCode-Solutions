2d65dcf0cb08dec0d2e13b38460174f35dd0abb0
Rohit Sinha <rohitsinha12043@gmail.com>
2025-09-05 17:30:25 +0000

    sync-leetcode: Runtime - 0 ms (100.00%), Memory - 23.9 MB (54.94%)

---
+++ 0283-move-zeroes/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int left = 0, right = 0, temp = 0;
+
+        while(right < nums.size()){
+            if(nums[right] != 0){
+                temp = nums[left];
+                nums[left] = nums[right];
+                nums[right] = temp;
+                left++;
+            }
+            right++;
+        }
+    }
+};
